8a99775d1b1928acf22e8be7580d86c3b1c2ec92
Adam Rosas <adamrosas27@gmail.com>
2024-02-10 20:28:35 -0800

    Refactor BinaryConversion class: Add necessary methods for handling various conversions

--- BinaryConversion.java
+++ BinaryConversion.java
@@ -1,6 +1,14 @@
 public class BinaryConversion {
 
-    public int conversion() {
+    public int decimalToBinary() {
+        return 0;
+    }
+
+    public int hexadecimalToBinary() {
+        return 0;
+    }
+
+    public int octalToBinary() {
         return 0;
     }
 }
