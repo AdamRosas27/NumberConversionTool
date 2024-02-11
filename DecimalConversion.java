f566e74191032ecc8a7a3164611f63f941d9c26a
Adam Rosas <adamrosas27@gmail.com>
2024-02-10 20:31:50 -0800

    Refactor DecimalConversion class: Add necessary methods for handling various conversions

---
+++ DecimalConversion.java
@@ -0,0 +1,14 @@
+public class DecimalConversion {
+
+    public int binaryToDecimal() {
+        return 0;
+    }
+
+    public int hexadecimalToDecimal() {
+        return 0;
+    }
+
+    public int octalToDecimal() {
+        return 0;
+    }
+}
