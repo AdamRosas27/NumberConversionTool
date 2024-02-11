25b4365843de4097c5cceeeaf7bcf2d0846ed113
Adam Rosas <adamrosas27@gmail.com>
2024-02-10 19:51:08 -0800

    Add error handling for user menu responses

--- Main.java
+++ Main.java
@@ -8,5 +8,30 @@ class Main {
 
         System.out.println("Number Conversion Tool:\n1. Convert\n2. Exit");
 
+        if (sc.hasNextInt()) {
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    System.out.println("Invalid input. Please try again.");
+            }
+        }
+
+        else {
+            String userInput = sc.next();
+
+            if (userInput.equalsIgnoreCase("convert")) {
+                return;
+            } else if (userInput.equalsIgnoreCase("exit")) {
+                return;
+            } else {
+                System.out.println("Invalid input. Please try again.");
+                ;
+            }
+        }
     }
 }
