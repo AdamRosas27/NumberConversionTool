05c31f58ce9ab6bd46765d24382438161a6f2d05
Adam Rosas <adamrosas27@gmail.com>
2024-02-10 20:33:55 -0800

    Refactor HexadecimalConversion class: Update method names

---
+++ HexadecimalConversion.java
@@ -0,0 +1,14 @@
+public class HexadecimalConversion {
+
+    public int binaryToHexadecimal() {
+        return 0;
+    }
+
+    public int decimalToHexadecimal() {
+        return 0;
+    }
+
+    public int octalToHexadecimal() {
+        return 0;
+    }
+}
