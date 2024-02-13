e60908edcc9dccf3b1e45a73520d81ea8d0026a2
Adam Rosas <adamrosas27@gmail.com>
2024-02-12 17:27:11 -0800

    Refactor menu options

--- Main.java
+++ Main.java
@@ -48,6 +48,7 @@ class Main {
             System.out.println("Octal: " + converter.hexadecimalToOctal());
           } else if (inputType == 4) {
             System.out.println("Enter octal value: ");
+            sc.nextLine();
             String octalValue = sc.nextLine();
             OctalConversion converter = new OctalConversion(octalValue);
             System.out.println("Binary: " + converter.octalToBinary());
@@ -74,16 +75,40 @@ class Main {
         String inputType = sc.next();
         if (inputType.equalsIgnoreCase("binary")) {
           System.out.println("Enter binary value: ");
-          sc.nextInt();
+          sc.nextLine();
+          String binaryString = sc.nextLine();
+
+          BinaryConversion converter = new BinaryConversion(binaryString);
+          System.out.println("Decimal: " + converter.binaryToDecimal());
+          System.out.println("Hexadecimal: " + converter.binaryToHexadecimal());
+          System.out.println("Octal: " + converter.binaryToOctal());
         } else if (inputType.equalsIgnoreCase("decimal")) {
           System.out.println("Enter decimal value: ");
-          sc.nextInt();
+          int decimalValue = sc.nextInt();
+          DecimalConversion converter = new DecimalConversion(decimalValue);
+          System.out.println("Binary: " + converter.decimalToBinary());
+          System.out.println(
+            "Hexadecimal: " + converter.decimalToHexadecimal()
+          );
+          System.out.println("Octal: " + converter.decimalToOctal());
         } else if (inputType.equalsIgnoreCase("hexadecimal")) {
           System.out.println("Enter hexadecimal value: ");
-          sc.nextInt();
+          sc.nextLine();
+          String hexadecimalValue = sc.nextLine();
+          HexadecimalConversion converter = new HexadecimalConversion(
+            hexadecimalValue
+          );
+          System.out.println("Binary: " + converter.hexadecimalToBinary());
+          System.out.println("Decimal: " + converter.hexadecimalToDecimal());
+          System.out.println("Octal: " + converter.hexadecimalToOctal());
         } else if (inputType.equalsIgnoreCase("octal")) {
           System.out.println("Enter octal value: ");
-          sc.nextInt();
+          sc.nextLine();
+          String octalValue = sc.nextLine();
+          OctalConversion converter = new OctalConversion(octalValue);
+          System.out.println("Binary: " + converter.octalToBinary());
+          System.out.println("Decimal: " + converter.octalToDecimal());
+          System.out.println("Hexadecimal: " + converter.octalToHexadecimal());
         } else {
           System.out.println("Invalid input. Please try again.");
         }
