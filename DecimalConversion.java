5ecbfb77e740dd31ef9ed07c0e3b3e01a228de21
Adam Rosas <adamrosas27@gmail.com>
2024-02-12 16:13:55 -0800

    Add conversion logic for decimal values

--- DecimalConversion.java
+++ DecimalConversion.java
@@ -1,14 +1,68 @@
 public class DecimalConversion {
 
-  public int decimalToBinary() {
-    return 0;
+  private int decimalValue;
+
+  public DecimalConversion(int decimalValue) {
+    this.decimalValue = decimalValue;
   }
 
-  public int decimalToHexadecimal() {
-    return 0;
+  public String decimalToBinary() {
+    StringBuilder binary = new StringBuilder();
+    int number = decimalValue;
+
+    if (number == 0) return "0";
+
+    while (number > 0) {
+      binary.insert(0, number % 2);
+      number /= 2;
+    }
+
+    return binary.toString();
   }
 
-  public int decimalToOctal() {
-    return 0;
+  public String decimalToHexadecimal() {
+    StringBuilder hexadecimal = new StringBuilder();
+    int number = decimalValue;
+    char[] hexCharacters = {
+      '0',
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      'A',
+      'B',
+      'C',
+      'D',
+      'E',
+      'F',
+    };
+
+    if (number == 0) return "0";
+
+    while (number > 0) {
+      hexadecimal.insert(0, hexCharacters[number % 16]);
+      number /= 16;
+    }
+
+    return hexadecimal.toString();
+  }
+
+  public String decimalToOctal() {
+    StringBuilder octal = new StringBuilder();
+    int number = decimalValue;
+
+    if (number == 0) return "0";
+
+    while (number > 0) {
+      octal.insert(0, number % 8);
+      number /= 8;
+    }
+
+    return octal.toString();
   }
 }
